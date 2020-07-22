f6f1562d141589ce72fb41a2a9bc882d66344309
Wescley Torres <66565807+wescleytorres@users.noreply.github.com>
2020-07-22 17:51:46 -0300

    novos exercicios

---
+++ Cap02-JSES6/Modulo-01/main.js
@@ -0,0 +1,88 @@
+/* OBJ USANDO YARN
+// Objeto Pai
+class List {
+    constructor() {
+        this.data = []
+    }
+
+    add(data) {
+        this.data.push(data)
+        console.log(this.data)
+    }
+}
+
+//Objeto herança 'filho' com variaçoes
+class TodoList extends List {
+    constructor() {
+       super()
+       
+       this.usuario = 'Jean'
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario)
+    }
+
+}
+
+var MinhaLista = new TodoList()
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo')
+}
+
+MinhaLista.mostraUsuario()
+
+// metodo Static , ja lhe devolve um retorno rapido
+class Matematica {
+    static soma(a, b) {
+        return a + b
+    }
+}
+
+console.log(Matematica.soma(1, 2))*/
+
+// const é como uma Tupla no Py: não pode ser alterada mas pode ter multação 
+/*const usuario = {nome: 'Jean'}
+
+usuario.nome = 'Cleiton'
+
+console.log(usuario)*/
+
+// o 'let' voce pode usar uma variavel não global
+/*function teste(x) {
+    let y = 2
+    if (x > 5) {
+        console.log(x, y)
+    }
+}
+
+teste(10)*/
+
+const arr = [1, 3, 4, 5, 8, 9]
+// o metodo MAP passa por cada item do vetor (como um FOR/WHILE) possivel chamar o INDEX tbm
+const newArr = arr.map(function(item, index) {
+    return item + index
+// item = 1, 3, 4, 5, 8, 9
+//index = 0, 1, 2, 3, 4, 5
+})
+
+console.log(newArr)
+
+
+const sum = arr.reduce(function(total, next) {
+    return total + next
+//primeira rodada: total = 0 next = 1
+//segunda rodada: total = 1 next = 3
+//terceira rodada: total = 4 next = 4
+//quart rodada: total = 8 next = 5... NO FINAL ME RETORNA O RESULTADO
+})
+
+console.log(sum)
+
+// o metodo FILTER vai retornar os itens True, conforme oq eu solicitar (exemp. retornar so num pares do vetor)
+const filter = arr.filter(function(item) {
+    return item % 2 === 0
+})
+
+console.log(filter)
