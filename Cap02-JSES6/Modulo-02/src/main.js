108bb905fffcd8e62614ab06c3f6be096d6d11fd
Wescley Torres <66565807+wescleytorres@users.noreply.github.com>
2020-07-27 17:34:17 -0300

    Async/await

--- Cap02-JSES6/Modulo-02/src/main.js
+++ Cap02-JSES6/Modulo-02/src/main.js
@@ -32,7 +32,7 @@ const minhaPromise = () => new Promise((resolve, reject) => {
 
 //o ASYNC substitui o .then/.cath
 // async function executaPromise() {
-//     // outra função so sera executada depois q a acima terminar devido o AWAIT 
+     // outra função so sera executada depois q a acima terminar devido o AWAIT 
 //     console.log( await minhaPromise())
 //     console.log( await minhaPromise())
 //     console.log( await minhaPromise())
@@ -48,20 +48,69 @@ const minhaPromise = () => new Promise((resolve, reject) => {
 
 // executaPromise()
 
+// import axios from 'axios'
+
+// class Api {
+//     static async getUserInfo(username) {
+//         // o TRY deve ser usado para se der erro, o CATCH respondera
+//         try {
+//             const response = await axios.get(`https://api.github.com/users/${username}`)
+            
+//             console.log(response)
+//         } catch (err) {
+//             console.warn('Erro na API')
+//         }
+        
+//     }
+// }
+
+// Api.getUserInfo('wescleytorres')
+
+
+// EXERCICIO
+// Funão delay aciona o .then após 1s
+
+// function umPorSegundo() {
+//  delay().then(() => {
+//  console.log('1s');
+
+
+// const delay = () => new Promise((resolve, reject) => setTimeout(() => { resolve(1)}, 1000));
+
+// async function umPorSegundo() {
+//     console.log( await delay()+0+'s')
+//     console.log( await delay()+1+'s')
+//     console.log( await delay()+2+'s')
+// }
+// umPorSegundo();
+
 import axios from 'axios'
 
-class Api {
-    static async getUserInfo(username) {
-        // o TRY deve ser usado para se der erro, o CATCH respondera
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`)
-            
-            console.log(response)
-        } catch (err) {
-            console.warn('Erro na API')
-        }
-        
-    }
-}
+// class API {    
+//         static async getUserFromGithub(user) {
+//             try {
+//                 const response = await axios.get(`https://api.github.com/users/${user}`)
+//             console.log(response.data)
+//             } catch (err) {
+//                 console.warn('Usuário não existe')
+//             }
+//     }
+// }
 
-Api.getUserInfo('wescleytorres')
+// API.getUserFromGithub('diego3g');
+// API.getUserFromGithub('diego3g124123');
+
+// class Github {
+//     static getRepositories(repo) {
+//         axios.get(`https://api.github.com/users/${repo}/repos`)
+//         .then(response => {
+//             console.log(response.data);
+//     })
+//     .catch(err => {
+//         console.log('Repositório não existe');
+//     })
+//     }
+// }
+
+// Github.getRepositories('rocketseat');
+// // Github.getRepositories('rocketseat/dslkvmskv');
